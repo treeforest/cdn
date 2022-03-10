004e3dd37c34e8fa9145b1c7666cf551ea510e5d
treeforest <1451670604@qq.com>
2022-03-10 15:17:40 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,27 @@
+module github.com/treeforest/cdn
+
+go 1.17
+
+require (
+	github.com/allegro/bigcache/v3 v3.0.2
+	github.com/gin-gonic/gin v1.7.7
+	github.com/syndtr/goleveldb v1.0.0
+)
+
+require (
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-playground/locales v0.13.0 // indirect
+	github.com/go-playground/universal-translator v0.17.0 // indirect
+	github.com/go-playground/validator/v10 v10.4.1 // indirect
+	github.com/golang/protobuf v1.3.3 // indirect
+	github.com/golang/snappy v0.0.0-20180518054509-2e65f85255db // indirect
+	github.com/json-iterator/go v1.1.9 // indirect
+	github.com/leodido/go-urn v1.2.0 // indirect
+	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/ugorji/go/codec v1.1.7 // indirect
+	golang.org/x/crypto v0.0.0-20200820211705-5c72a883971a // indirect
+	golang.org/x/sys v0.0.0-20200116001909-b77594299b42 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,164 @@
+package main
+
+import (
+	"fmt"
+	"github.com/allegro/bigcache/v3"
+	"github.com/gin-gonic/gin"
+	"github.com/syndtr/goleveldb/leveldb"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+	"time"
+)
+
+var (
+	LevelDB *leveldb.DB
+	Cache   *bigcache.BigCache
+)
+
+func init() {
+	Cache, _ = bigcache.NewBigCache(bigcache.DefaultConfig(10 * time.Minute))
+	LevelDB, _ = leveldb.OpenFile("leveldb", nil)
+}
+
+func main() {
+	defer func() {
+		_ = Cache.Close()
+		_ = LevelDB.Close()
+	}()
+
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+	r.POST("/upload", UploadHandler)
+	r.GET("/download/:filename", DownloadHandler)
+	r.POST("/delete/:filename", DeleteHandler)
+	r.GET("/all", func(c *gin.Context) {
+		snapshot, _ := LevelDB.GetSnapshot()
+		defer snapshot.Release()
+		iter := snapshot.NewIterator(nil, nil)
+		defer iter.Release()
+		files := make([]string, 0)
+		for iter.Next() {
+			key := iter.Key()
+			files = append(files, string(key))
+		}
+		c.JSON(http.StatusOK, gin.H{"code": 0, "files": files})
+	})
+
+	log.Fatal(r.Run(":5999"))
+}
+
+func UploadHandler(c *gin.Context) {
+	fileHeader, err := c.FormFile("file")
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": -1, "error": "param error"})
+		return
+	}
+
+	if fileHeader.Size > 1024*1024*10 {
+		c.JSON(http.StatusBadRequest, gin.H{"code": -1, "error": "file size exceeds 10MB"})
+		return
+	}
+
+	file, err := fileHeader.Open()
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"code": -1, "error": err.Error()})
+		return
+	}
+	defer file.Close()
+
+	data, _ := ioutil.ReadAll(file)
+	filename := fileHeader.Filename
+
+	exist, _ := LevelDB.Has([]byte(filename), nil)
+	if exist {
+		c.JSON(http.StatusOK, gin.H{"code": -1, "error": "filename already exist"})
+		return
+	}
+
+	_ = LevelDB.Put([]byte(filename), data, nil)
+	c.JSON(http.StatusOK, gin.H{"code": 0})
+}
+
+func DeleteHandler(c *gin.Context) {
+	filename := c.Param("filename")
+	if filename == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"code": -1, "error": "filename is null"})
+		return
+	}
+
+	_ = Cache.Delete(filename)
+	_ = LevelDB.Delete([]byte(filename), nil)
+	c.JSON(http.StatusOK, gin.H{"code": 0})
+}
+
+func DownloadHandler(c *gin.Context) {
+	filename := c.Param("filename")
+	if filename == "" {
+		c.JSON(http.StatusBadRequest, gin.H{"code": -1, "error": "filename is null"})
+		return
+	}
+
+	data, err := Cache.Get(filename)
+	if err != nil {
+		if err != bigcache.ErrEntryNotFound {
+			c.JSON(http.StatusInternalServerError, gin.H{"code": -2, "error": "cache error"})
+			return
+		}
+
+		data, err = LevelDB.Get([]byte(filename), nil)
+		if err != nil {
+			if err != leveldb.ErrNotFound {
+				c.JSON(http.StatusInternalServerError, gin.H{"code": -3, "error": "db error"})
+			} else {
+				c.JSON(http.StatusOK, gin.H{"code": 1, "message": "not found"})
+			}
+			return
+		}
+
+		_ = Cache.Set(filename, data)
+	}
+
+	delivery(c, filename, data)
+}
+
+func delivery(c *gin.Context, filename string, content []byte) {
+	contentType := ""
+	s := strings.Split(filename, ".")
+	fileType := s[len(s)-1]
+
+	switch fileType {
+	case "jpg":
+		contentType = "image/jpeg"
+	case "png":
+		contentType = "image/png"
+	case "img":
+		contentType = "application/x-img"
+	case "jpe", "jpeg":
+		contentType = "image/jpeg"
+	case "gif":
+		contentType = "image/gif"
+	case "txt":
+		contentType = "text/plain"
+	case "zip":
+		contentType = "application/zip"
+	case "pbf":
+		contentType = "application/pdf"
+	case "word":
+		contentType = "application/msword"
+	default:
+		contentType = "application/octet-stream"
+	}
+
+	c.Writer.WriteHeader(http.StatusOK)
+	c.Header("Content-Disposition", fmt.Sprintf(`attachment; filename="%s"`, url.QueryEscape(filename)))
+	c.Header("Content-Type", contentType)
+	c.Header("Accept-Length", fmt.Sprintf("%d", len(content)))
+	c.Header("Filename", filename)
+	_, _ = c.Writer.Write(content)
+}
